7663194d9ea356b5fde927cc704157e0246dc354
armyworm <baseballki66@gmail.com>
2015-11-02 23:05:48 +0800

    first sign design

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :title, :description, :image_url, :price, :categiry, :subcategory, :created_at, :updated_at
---
+++ db/migrate/20151102142625_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :image_url
+      t.integer :price
+      t.string :categiry
+      t.string :subcategory
+
+      t.timestamps null: false
+    end
+  end
+end
